604f812cae67e6ce2fe34c0c1b53ede5d5e196bd
mirickrompiric <mirjanaglavonjic@gmail.com>
2017-02-25 12:05:52 +0000

    play with header and footer

---
+++ public/myscript.js
@@ -0,0 +1,15 @@
+var houseprice = 20;
+var flatprice = 11;
+var trialprice = 1;
+var disprice = 19;
+var currency = "£";
+
+
+function elements() {
+    "use strict";
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+}
+
+
+
