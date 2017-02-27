f4a44c4cf91f2ac843dd333ba2d078ab1266480d
Mimi <mirjana.glavonjic@tradecore.com>
2017-02-27 09:59:39 +0000

    Add thank you pages

--- public/myscript.js
+++ public/myscript.js
@@ -1,3 +1,5 @@
+/*global $ */
+
 var houseprice = 20;
 var flatprice = 11;
 var trialprice = 1;
@@ -5,11 +7,15 @@ var disprice = 19;
 var currency = "£";
 
 
-function elements() {
+$(function () {
     "use strict";
     $("#header").load("header.html");
     $("#footer").load("footer.html");
-}
+    $("#meta").load("meta.html");
+});
 
 
-
+$(document).on('click', '#resetpass', function () {
+    "use strict";
+    window.location = 'forgot-pass-email.html';
+});
