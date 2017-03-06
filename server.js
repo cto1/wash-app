7891ccec930069c1ab6a85bbdee81ae283480ac0
cto1 <a.kudic@gmail.com>
2017-03-06 23:32:30 +0000

    Not working checkin

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-const keyPublishable = process.env.PUBLISHABLE_KEY || "pk_test_PvPL3W94KCJmEXmKLpkOzxJE";
-const keySecret = process.env.SECRET_KEY  || "sk_test_Wxt8a3lgXbCdSvRJpAXsBVRq";
+const braintree = require("braintree");
+
+const gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "xpwyrfws6pc5pfp6",
+  publicKey: "t6htsgqtp3w5tjgy",
+  privateKey: "b50a79443471ba287b90e2895ff49ba5"
+});
 
 const port = process.env.PORT || 3000;
 var app = express();
@@ -27,6 +33,13 @@ app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
 
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -37,12 +50,7 @@ hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
-app.get('/', (req, res) => {
-  res.render('home.hbs', {
-    pageTitle: 'Home Page',
-    welcomeMessage: 'Welcome to my website'
-  });
-});
+
 
 // /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
@@ -65,10 +73,36 @@ app.get('/service', (req, res) => {
 }
 });
 
+app.get("/client_token", function (req, res) {
+  gateway.clientToken.generate({}, function (err, response) {
+    res.send(response.clientToken);
+  });
+});
+
 app.get('/pay', (req, res) => {
-  res.render('pay.hbs', {
-    pageTitle: 'Checkout Page',
-    welcomeMessage: 'Welcome to my website'
+  gateway.clientToken.generate({}, function (err, response) {
+    res.render('pay.hbs', {
+      pageTitle: 'Checkout Page',
+      welcomeMessage: 'Please enter you payment details',
+      clientToken: response.clientToken
+    });
+  });
+});
+
+app.post("/checkout", function (req, res) {
+  var nonceFromTheClient = req.body.payment_method_nonce;
+  // Use payment method nonce here
+  console.log(req.body);
+
+
+  gateway.transaction.sale({
+  amount: "10.00",
+  paymentMethodNonce: nonceFromTheClient,
+  options: {
+    submitForSettlement: true
+    }
+  }, function (err, result) {
+    console.log(result);
   });
 });
 
